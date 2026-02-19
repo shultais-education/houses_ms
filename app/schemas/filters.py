b5cb58a1bea9412133979e082125f8b9f5588854
Nikita Shultais <nikita@shultais.ru>
2026-02-19 18:29:07 +0200

    Перенес логику фильтрации в фильтры

--- app/schemas/filters.py
+++ app/schemas/filters.py
@@ -1,6 +1,9 @@
-from typing import Optional
-from fastapi import Query
 from dataclasses import dataclass
+from typing import Optional
+
+from app.models import House
+from fastapi import Query
+from sqlmodel import or_
 
 
 @dataclass
@@ -8,3 +11,18 @@ class HouseFilters:
     search: Optional[str] = Query(None, min_length=3, title="Поиск по названию")
     min_price: Optional[int] = Query(None, ge=0, title="Минимальная цена")
     max_price: Optional[int] = Query(None, ge=0, title="Максимальная цена")
+
+    def build_query(self, base_query):
+        query = base_query
+
+        if self.search:
+            query = query.where(or_(House.description.icontains(self.search), House.name.icontains(self.search)))
+
+        if self.min_price is not None:
+            query = query.where(House.price >= self.min_price)
+
+        if self.max_price is not None:
+            query = query.where(House.price <= self.max_price)
+
+        return query
+
