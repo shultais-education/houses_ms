99b5d9901d1faf241f237f4099092f7cd2ff89d1
Nikita Shultais <nikita@shultais.ru>
2026-02-19 18:57:57 +0200

    Добавил репозиторий для работы с домами

---
+++ app/repositories/houses.py
@@ -0,0 +1,12 @@
+from app.models import House
+from app.repositories.db import DBRepository
+from sqlalchemy import Sequence
+
+
+class HouseRepository(DBRepository):
+
+    async def get_house(self, house_id: int) -> House:
+        return await self.get_one(id_=house_id)
+
+    async def get_houses(self, filters=None, order_by="id", order="asc") -> Sequence[House]:
+        return await self.get_many(filters=filters, order_by=order_by, order=order)
