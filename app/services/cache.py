1712ffa85bf9e12b1cf28ac8e52fb2f3aaff3336
Nikita Shultais <nikita@shultais.ru>
2026-02-19 19:09:02 +0200

    Сервис кэширования

--- app/services/cache.py
+++ app/services/cache.py
@@ -1,4 +1,5 @@
 from redis.asyncio import Redis
+import json
 
 
 class CacheService:
@@ -6,8 +7,15 @@ class CacheService:
     def __init__(self, redis_client: Redis):
         self.redis_client = redis_client
 
-    async def get(self, key):
-        ...
+    async def get(self, key, default=None):
+        data = await self.redis_client.get(key)
+
+        if data is not None:
+            return json.loads(data)
+
+        return default
 
     async def set(self, key, value, ttl=None):
-        ...
+        value = json.dumps(value)
+        await self.redis_client.setex(name=key, value=value, time=ttl)
+        return True
