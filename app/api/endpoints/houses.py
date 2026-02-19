baf2d7a3a4ecba28eec82544080875741da69228
Nikita Shultais <nikita@shultais.ru>
2026-02-19 12:54:30 +0200

    Базовая логика доступа к домам

--- app/api/endpoints/houses.py
+++ app/api/endpoints/houses.py
@@ -1,14 +1,18 @@
 from fastapi import APIRouter
-
+from app.data import houses_list
 
 houses_router = APIRouter(prefix="/houses", tags=["houses"])
 
 
 @houses_router.get("/")
 async def houses():
-    return {"message": "Вывод домов"}
+    return houses_list
 
 
 @houses_router.get("/{house_id}")
 async def house_detail(house_id: int):
-    return {"message": f"Вывод дома {house_id}"}
+    for house in houses_list:
+        if house["id"] == house_id:
+            return house
+
+    return {"message": f"Дом не найден"}
