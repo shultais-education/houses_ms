82683c7a8934afad0fcf6feb590edef20058e926
Nikita Shultais <nikita@shultais.ru>
2026-02-19 13:18:38 +0200

    Добавил фильтрацию по цене

--- app/api/endpoints/houses.py
+++ app/api/endpoints/houses.py
@@ -1,16 +1,27 @@
 from fastapi import APIRouter
 from fastapi import HTTPException
+from fastapi import Query
 from app.data import houses_list
 from app.schemas.house import HouseDetailSchema, HouseItemSchema
-from typing import List
+from typing import List, Optional
 
 
 houses_router = APIRouter(prefix="/houses", tags=["houses"])
 
 
 @houses_router.get("/", response_model=List[HouseItemSchema])
-async def get_houses():
+async def get_houses(
+        min_price: Optional[int] = Query(None),
+        max_price: Optional[int] = Query(None)):
+
     houses = [house for house in houses_list if house["active"]]
+
+    if min_price is not None:
+        houses = [house for house in houses if house["price"] >= min_price]
+
+    if max_price is not None:
+        houses = [house for house in houses if house["price"] <= max_price]
+
     return houses
 
 
