b5d67b6a4c83921e62750ea4937b15893539890d
Nikita Shultais <nikita@shultais.ru>
2026-02-19 19:21:54 +0200

    Добавил возврат одного дома

--- app/services/houses.py
+++ app/services/houses.py
@@ -12,10 +12,11 @@ class HouseService:
         self.cache = cache
 
     async def get_house(self, house_id: int) -> House:
-        ...
+        return await self.repository.get_house(house_id=house_id)
 
     async def get_active_house(self, house_id: int) -> House:
-        ...
+        # Фильтр по активному дому
+        return await self.repository.get_house(house_id=house_id)
 
     async def get_houses(self, filters=None, order_by="id", order="asc") -> Sequence[House]:
         return await self.repository.get_houses(filters=filters, order_by=order_by, order=order)
