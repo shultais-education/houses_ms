aba48f053e8994c9dd1012fb7e878c77c203dec5
Nikita Shultais <nikita@shultais.ru>
2026-02-19 16:40:41 +0200

    Добавил описание приложение и эндпоинтов

--- app/api/endpoints/houses.py
+++ app/api/endpoints/houses.py
@@ -11,24 +11,32 @@ houses_router = APIRouter(prefix="/houses", tags=["houses"])
 
 
 SortField = Literal["id", "price", "name"]
-SortOrder= Literal["asc", "desc"]
+SortOrder = Literal["asc", "desc"]
 
 
-@houses_router.get("/", response_model=List[HouseItemSchema])
+@houses_router.get("/", response_model=List[HouseItemSchema], summary="Возвращает дома", description="Возвращает список активных домов")
 async def get_houses(
         session: DBSessionDep,
-        min_price: Optional[int] = Query(None, ge=0),
-        max_price: Optional[int] = Query(None, ge=0),
-        order_by: Optional[SortField] = Query("id"),
-        order: Optional[SortOrder] = Query("asc")):
+        min_price: Optional[int] = Query(None, ge=0, title="Минимальная цена"),
+        max_price: Optional[int] = Query(None, ge=0, title="Максимальная цена"),
+        order_by: Optional[SortField] = Query("id", title="Поля сортировки", description="Допустимые значения: id, price, name"),
+        order: Optional[SortOrder] = Query("asc", title="Направление сортировки", description="Допустимые значения: asc, desc")
+    ):
 
     houses = crud_house.get_filtered_active_houses(session, min_price=min_price, max_price=max_price, order_by=order_by, order=order)
 
     return houses
 
 
-@houses_router.get("/{house_id}", response_model=HouseDetailSchema)
+@houses_router.get("/{house_id}", response_model=HouseDetailSchema, summary="Возвращает дом")
 async def get_house(session: DBSessionDep, house_id: int):
+    """
+    Возвращает подробную информацию о доме:
+       - **id**: идентификатор
+       - **name**: название дома
+       - **description**: короткое описание
+       - **price**: цена дома в рублях
+    """
     house = crud_house.get_house(session=session, house_id=house_id)
 
     if house is not None:
