3d400ae1da57b5d9d5f8a06caf09cd174eb57621
Nikita Shultais <nikita@shultais.ru>
2026-02-24 13:43:40 +0200

    Удали старый код

--- app/api/endpoints/houses.py
+++ app/api/endpoints/houses.py
@@ -4,7 +4,6 @@ from fastapi import Query
 from app.schemas.house import HouseDetailSchema, HouseItemSchema
 from typing import List, Optional, Literal
 from app.api.dependencies.houses import HouseFiltersDep, HouseServiceDep
-from app.api.dependencies.cache import CacheServiceDep
 
 
 houses_router = APIRouter(prefix="/houses", tags=["houses"])
@@ -17,21 +16,17 @@ SortOrder = Literal["asc", "desc"]
 @houses_router.get("", response_model=List[HouseItemSchema], summary="Возвращает дома", description="Возвращает список активных домов")
 async def get_houses(
         house_service: HouseServiceDep,
-        cache: CacheServiceDep,
         filters: HouseFiltersDep,
         order_by: Optional[SortField] = Query("id", title="Поля сортировки", description="Допустимые значения: id, price, name"),
         order: Optional[SortOrder] = Query("asc", title="Направление сортировки", description="Допустимые значения: asc, desc")
     ):
 
-    await cache.set(key="TEST-50", value="TEST VALUE-60", ttl=60)
     houses = await house_service.get_houses(filters=filters, order_by=order_by, order=order)
     return houses
 
 
 @houses_router.get("/{house_id}", response_model=HouseDetailSchema, summary="Возвращает дом")
-async def get_house(
-        cache: CacheServiceDep,
-        house_service: HouseServiceDep, house_id: int):
+async def get_house(house_service: HouseServiceDep, house_id: int):
     """
     Возвращает подробную информацию о доме:
        - **id**: идентификатор
@@ -40,7 +35,6 @@ async def get_house(
        - **price**: цена дома в рублях
     """
     house = await house_service.get_active_house(house_id=house_id)
-    value = await cache.get(key="TEST-50")
 
     if not house:
         raise HTTPException(status_code=404, detail=f"House {house_id} not found")
