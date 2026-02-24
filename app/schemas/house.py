dd302af427e3e9f733dcbd32add3fc142d835aa1
Nikita Shultais <nikita@shultais.ru>
2026-02-24 13:35:33 +0200

    Добавил новые поля в схему HouseDetail

--- app/schemas/house.py
+++ app/schemas/house.py
@@ -15,6 +15,14 @@ class HouseDetailSchema(BaseModel):
     description: str = Field(description='Краткое описание')
     price: int = Field(description='Цена в рублях', examples=['5000', '10000'])
 
+    text: str
+    deposit: int | None
+    square: int | None
+    rooms: int
+    bathrooms: int
+    free_parking: bool
+    pets_allowed: bool | None
+
 
 class HouseItemSchema(BaseModel):
     id: int
