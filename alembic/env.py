dc1d7a43d151893b75657be81b9b9ade64ec8f8e
Nikita Shultais <nikita@shultais.ru>
2026-02-24 13:26:09 +0200

    Улучшил модель House

--- alembic/env.py
+++ alembic/env.py
@@ -64,6 +64,7 @@ def run_migrations_offline() -> None:
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
+        compare_server_default=True,
         dialect_opts={"paramstyle": "named"}
     )
 
@@ -87,7 +88,8 @@ def run_migrations_online() -> None:
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
-            target_metadata=target_metadata
+            target_metadata=target_metadata,
+            compare_server_default=True,
         )
 
         with context.begin_transaction():
--- app/models/house.py
+++ app/models/house.py
@@ -6,14 +6,35 @@ class House(SQLModel, table=True):
     __tablename__ = "houses"
 
     id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
-    description: str = Field(default="")
-    price: int = Field(default=0, index=True)
-    active: bool = Field(default=True, index=True)
+    name: str = Field(
+        sa_column=sa.Column(sa.String, nullable=False, index=True)
+    )
+    description: str = Field(
+        default="",
+        sa_column=sa.Column(sa.String, server_default=sa.text("''"), nullable=False)
+    )
+    price: int = Field(
+        default=0,
+        sa_column=sa.Column(sa.Integer, server_default=sa.text("0"), nullable=False, index=True)
+    )
+    active: bool = Field(
+        default=False,
+        sa_column=sa.Column(sa.Boolean, server_default=sa.text("FALSE"), nullable=False, index=True)
+    )
 
-    square: int | None = Field(default=None)
-    rooms: int | None = Field(default=1)
-    bathrooms: int | None = Field(default=1, nullable=True)
+    # Дополнительные атрибуты
+    square: int = Field(
+        default=None,
+        sa_column=sa.Column(sa.Integer, nullable=True)
+    )
+    rooms: int = Field(
+        default=1,
+        sa_column=sa.Column(sa.Integer, server_default=sa.text("1"), nullable=False)
+    )
+    bathrooms: int = Field(
+        default=1,
+        sa_column=sa.Column(sa.Integer, server_default=sa.text("1"), nullable=False)
+    )
     free_parking: bool = Field(
         default=False,
         sa_column=sa.Column(sa.Boolean, server_default=sa.text("FALSE"), nullable=False))
