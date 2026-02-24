313d3c9f502150b8c06e306169757e614de0f4ad
Nikita Shultais <nikita@shultais.ru>
2026-02-24 13:38:50 +0200

    Добавил фильтр по домашним животным

--- app/schemas/filters.py
+++ app/schemas/filters.py
@@ -11,9 +11,13 @@ class HouseFilters:
     search: Optional[str] = Query(None, min_length=3, title="Поиск по названию")
     min_price: Optional[int] = Query(None, ge=0, title="Минимальная цена")
     max_price: Optional[int] = Query(None, ge=0, title="Максимальная цена")
+    pets: Optional[bool] = Query(None, title="Разрешено с домашними животными")
 
     def build_query(self, base_query):
         query = base_query
+
+        if self.pets is not None:
+            query = query.where(House.pets_allowed == self.pets)
 
         if self.search:
             query = query.where(or_(House.description.icontains(self.search), House.name.icontains(self.search)))
