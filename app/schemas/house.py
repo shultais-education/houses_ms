bd2249c423996ea05498e1d87a00b8d415bb4198
Nikita Shultais <nikita@shultais.ru>
2026-03-03 11:55:35 +0200

    Улучшил проверку входящих данных для дома.

--- app/schemas/house.py
+++ app/schemas/house.py
@@ -28,14 +28,14 @@ class HouseCreateSchema(BaseModel):
 
 
 class HouseUpdateSchema(BaseModel):
-    name: Optional[str] = Field(default=None, min_length=3, max_length=100)
-    description: Optional[str] = None
-    price: Optional[int] = None
-    text: Optional[str] = None
-    deposit: Optional[int] = None
-    square: Optional[int] = None
-    rooms: Optional[int] = None
-    bathrooms: Optional[int] = None
-    free_parking: Optional[bool] = None
-    pets_allowed: Optional[bool] = None
-    active: Optional[bool] = None
+    name: Optional[str] = Field(default=None, min_length=3, max_length=100, description="Название")
+    description: Optional[str] = Field(default=None, min_length=10, max_length=255, description="Описание")
+    price: Optional[int] = Field(default=None, ge=0, description="Цена")
+    text: Optional[str] = Field(default=None, min_length=100, description="Текст")
+    deposit: Optional[int] = Field(default=None, ge=0, description="Депозит")
+    square: Optional[int] = Field(default=None, gt=0, description="Площадь")
+    rooms: Optional[int] = Field(default=None, gt=0, description="Комнат")
+    bathrooms: Optional[int] = Field(default=None, gt=0, description="Ванных комнат")
+    free_parking: Optional[bool] = Field(default=None, description="Бесплатная парковка")
+    pets_allowed: Optional[bool] = Field(default=None, description="Разрешено с домашними животными")
+    active: Optional[bool] = Field(default=None, description="Активный (доступен для бронирования)")
